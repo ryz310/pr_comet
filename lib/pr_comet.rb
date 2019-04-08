b0b05e30a1c667c15394c4b0255691c792b96ba4
ryz310 <ryz310@gmail.com>
2019-04-08 21:44:47 +0900

    Run $ bundle gem pr_comet

---
+++ lib/pr_comet.rb
@@ -0,0 +1,5 @@
+require "pr_comet/version"
+
+module PrComet
+  # Your code goes here...
+end
---
+++ spec/pr_comet_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe PrComet do
+  it "has a version number" do
+    expect(PrComet::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
